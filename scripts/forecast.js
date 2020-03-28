cf112981410f9c9c2f77d65496c9d8694c1e5947
KamilaMania <mania.kamika@icloud.com>
2020-03-28 16:49:12 +0100

    cors

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -5,7 +5,7 @@ const key = "1DU7sAz41eTDuUaY04pJPicUbdaOQ551";
 const getWeather = async id => {
   const base = "http://dataservice.accuweather.com/currentconditions/v1/";
   const query = `${id}?apikey=${key}`;
-  const response = await fetch(base + query);
+  const response = await fetch(base + query, { mode: "cors" });
   const data = await response.json();
   return data[0];
 };
@@ -13,7 +13,7 @@ const getWeather = async id => {
 const getCity = async city => {
   const base = " http://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
-  const response = await fetch(base + query);
+  const response = await fetch(base + query, { mode: "cors" });
   const data = await response.json();
 
   return data[0];
